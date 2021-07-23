5418b0f9a36e4cef2564c34f00a89072452d4d76
Oleh Yastrebov <olwg199@gmail.com>
2021-07-22 22:11:43 -0400

    Add new design

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 require("dotenv").config();
 
 const express = require("express");
-const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const _ = require('lodash.capitalize');
@@ -11,11 +10,10 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect(process.env.ATLAS_URI,
-    { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+mongoose.connect(process.env.ATLAS_URI, { useNewUrlParser: true });
 
 const itemSchema = {
     name: {
